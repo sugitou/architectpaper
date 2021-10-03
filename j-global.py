bbffbd7267e00c0688e9dd632a692499e8163830
sugitou <sugi_tou108@yahoo.co.jp>
2021-10-03 22:04:04 +0900

    取得完了

--- j-global.py
+++ j-global.py
@@ -54,25 +54,61 @@ def main():
     # ログファイル作成
     log_path = f'{crdir("log")}/log_{now}.log'
     # 空のDataFrame作成
-    df = pd.DataFrame()
+    # df = pd.DataFrame()
     # 件数カウンター作成
     job_num = 0
+    # リンク一覧
+    elem_urls = []
+    # ページ数カウント
+    i = 0
+    # DataFrameに入れるリスト
+    dict_array = []
 
-    titles = driver.find_elements_by_class_name('listbox_title_a')    # タイトル
-    print('title：', titles[0].text)
-    source = driver.find_elements_by_css_selector("div.listbox_info1 > div")
-    source_even = source[0::2]    # 著者
-    source_odd = source[1::2]    # 発行媒体
-    # ページがなくなったら止める
-    # if len(titles) == 0:
-    #     print(str(i))
-    #     break
+    while True:
+        try:
+            titles = driver.find_elements_by_class_name('listbox_title_a')    # タイトル
+            source = driver.find_elements_by_css_selector("div.listbox_info1 > div")
+            source_even = source[0::2]    # 著者
+            source_odd = source[1::2]    # 発行媒体
+            # ページがなくなったら止める
+            if len(titles) == 0:
+                print('これ以上ページがありません。')
+                print('次に論文詳細ページをスクレイピングします。')
+                break
 
-    for title, writer, source_title in zip(titles, source_even, source_odd):
-        t_title = title.text
-        t_writer = writer.text
-        t_source_title = source_title.text
-        source_page_link = title.get_attribute("href")
+            for title, writer, source_title in zip(titles, source_even, source_odd):
+                elem_urls.append(title.get_attribute("href"))
+                
+                # 件数をカウント
+                job_num += 1
+                out_num = f'{job_num}件目'
+                logfile(log_path, out_num)
+
+                dict_array.append(
+                    {"論文の発行年": '', 
+                    "論文が発行された媒体": source_title.text,
+                    "媒体の巻号": '',
+                    "論文タイトル": title.text,
+                    "著者": writer.text,
+                    "媒体のページ数": ''})
+            
+            page_param = {
+                "category" : 2,
+                "keyword" : 'X0722A',
+                "page" : i
+            }
+            next = requests.get("https://jglobal.jst.go.jp/search/articles#" + json.dumps(page_param))
+            driver.get(next.url)
+            
+        except Exception as e:
+            er = f'{job_num}件目でエラーが発生しました。'
+            logfile(log_path, er)
+            print(e)
+            continue
+        
+        i += 1
+    
+    for n, source_page_link in enumerate(elem_urls):
         driver.get(source_page_link)
         detail = driver.find_element_by_class_name('search_detail')
         details = detail.text.split('\n')
@@ -86,81 +122,16 @@ def main():
                 source_page = source_npy[1].split('： ')[1]
                 year = source_npy[2].split('： ')[1]
         
-        # 件数をカウント
-        job_num += 1
-        out_num = f'{job_num}件目'
-        logfile(log_path, out_num)
+        dict_array[n]["論文の発行年"] = year
+        dict_array[n]["媒体の巻号"] = source_number
+        dict_array[n]["媒体のページ数"] = source_page
 
-        df = df.append(
-            {"論文の発行年": year, 
-            "論文が発行された媒体": t_source_title,
-            "媒体の巻号": source_number,
-            "論文タイトル": t_title,
-            "著者": t_writer,
-            "媒体のページ数": source_page}, 
-            ignore_index=True)
-        
-        driver.back()
-
-    i = 2
-
-    # while True:
-    #     try:
-    #         titles = driver.find_elements_by_class_name('listbox_title_a')    # タイトル
-    #         source = driver.find_elements_by_css_selector("div.listbox_info1 > div")
-    #         source_even = source[0::2]    # 著者
-    #         source_odd = source[1::2]    # 発行媒体
-    #         # ページがなくなったら止める
-    #         if len(titles) == 0:
-    #             print(str(i))
-    #             break
-
-    #         for title, writer, source_title in zip(titles, source_even, source_odd):
-    #             source_page_link = title.get_attribute("href")
-    #             driver.get(source_page_link)
-    #             detail = driver.find_element_by_class_name('search_detail')
-    #             details = detail.text.split('\n')
-    #             source_number = ''
-    #             source_page = ''
-    #             year = ''
-    #             for row in details:
-    #                 if row.startswith('巻：'):
-    #                     source_npy = row.split('  ')
-    #                     source_number = source_npy[0].split('： ')[1]
-    #                     source_page = source_npy[1].split('： ')[1]
-    #                     year = source_npy[2].split('： ')[1]
-    #             driver.back()
-
-    #             # 件数をカウント
-    #             job_num += 1
-    #             out_num = f'{job_num}件目'
-    #             logfile(log_path, out_num)
-
-    #             df = df.append(
-    #                 {"論文の発行年": year, 
-    #                 "論文が発行された媒体": source_title.text,
-    #                 "媒体の巻号": source_number,
-    #                 "論文タイトル": title.text,
-    #                 "著者": writer.text,
-    #                 "媒体のページ数": source_page}, 
-    #                 ignore_index=True)
-
-    #     except Exception as e:
-    #         er = f'{job_num}件目でエラーが発生しました。'
-    #         logfile(log_path, er)
-    #         print(e)
-    #         continue
-
-        # page_param = {
-        #     "category" : 2,
-        #     "keyword" : 'X0722A',
-        #     "page" : i
-        # }
-        # next = requests.get("https://jglobal.jst.go.jp/search/articles#" + json.dumps(page_param))
-        # driver.get(next.url)
-        
-        # i += 1
-    pprint.pprint(df)
+    df = pd.DataFrame(dict_array)
+    # print(len(elem_urls))
+    # print(len(df))
+    csv_path = f'{crdir("csv")}/csv_{now}.csv'
+    # csvファイルに取得データを出力
+    df.to_csv(csv_path)
 
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
